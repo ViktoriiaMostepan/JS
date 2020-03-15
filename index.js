d266ea955510a973d8a65ecbf4bbccc81abde359
Vika Mostepan <viktoriamostepan@gmail.com>
2020-03-15 15:07:36 +0200

    Function Search

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// *****Task 1*****
+
 // Variant 1
 // console.log(Math.pow(2, 6));
 
@@ -17,6 +19,8 @@
 //   return pow;
 // }
 // console.log(pow(20, 4));
+
+// *****Task 2*****
 
 // const inputValue = Number(prompt("Введите число от 1 до 10"));
 // const romeChars = {
@@ -56,23 +60,94 @@
 // // console.log(lookup.X);
 // console.log(convertToRoman(35));
 
-const word = "VVVoesnesoooooonbvnbv";
+// *****Task 3****
 
+// const word = "joavoaScoorooioopt";
 // (function symbolsCounter(word) {
 //   word = word.toLowerCase();
 //   console.log(Number(word.split("o").length - 1));
 // })(word);
 
-(function symbolDelater(word) {
-  word = word.toLowerCase();
-  let splitWord = word.split("");
-  let wordArr = [];
-  for (let i = 0; i <= word.length - 1; i++) {
-    if (splitWord[i] !== "o") {
-      wordArr.push(splitWord[i]);
-    } else {
-      continue;
-    }
+// function with cycle
+// function symbolDelater(word) {
+//   word = word.toLowerCase();
+//   let splitWord = word.split("");
+
+//   let wordArr = [];
+//   for (let i = 0; i <= word.length - 1; i++) {
+//     if (splitWord[i] !== "o") {
+//       wordArr.push(splitWord[i]);
+//     } else {
+//       continue;
+//     }
+//   }
+//   return wordArr.join("");
+// }
+
+// function with filter
+// function symbolDelaterFilter(word) {
+//   word = word.toLowerCase();
+//   let splitWord = word.split("");
+
+//   const newArr = splitWord.filter(symbol => symbol !== "o");
+
+//   return newArr.join("");
+// }
+
+// function with reduce
+// function symbolDelaterReduce(word) {
+//   word = word.toLowerCase();
+//   let splitWord = word.split("");
+
+//   let newWord = splitWord.reduce((acc, symbol) => {
+//     if (symbol !== "o") {
+//       acc += symbol;
+//     }
+//     return acc;
+//   }, "");
+
+//   return newWord;
+// }
+
+// console.log(symbolDelater(word));
+// console.log(symbolDelaterFilter(word));
+// console.log(symbolDelaterReduce(word));
+
+// *****Task 4*****
+
+const authors = [
+  {
+    id: 1,
+    name: "Айзек",
+    surname: "Азимов"
+  },
+  {
+    id: 2,
+    name: "Рэй",
+    surname: "Брэдбери"
+  },
+  {
+    id: 3,
+    name: "Роберт",
+    surname: "Хайнлайн"
+  },
+  {
+    id: 4,
+    name: "Филип",
+    surname: "Дик"
+  },
+  {
+    id: 5,
+    name: "Дэн",
+    surname: "Симмонс"
   }
-  console.log(wordArr.join(""));
-})(word);
+];
+
+const word = "симмонс";
+(function search(authors, word) {
+  const outputObj = authors.filter(
+    item =>
+      item.name.toLowerCase() === word || item.surname.toLowerCase() === word
+  );
+  outputObj.length > 0 ? console.log(outputObj) : console.log("совпадений нет");
+})(authors, word);
