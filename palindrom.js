45c1cecebb2cc00b16773dc1da0efe9894a3b9c3
Vika Mostepan <viktoriamostepan@gmail.com>
2020-04-26 23:40:03 +0300

    new variants

--- palindrom.js
+++ palindrom.js
@@ -1,9 +1,8 @@
 'use strict';
 
-let string = 'alala';
+const string = 'assassa';
 
 const pal = string => {
-  console.log(string);
   if (string.length <= 1) {
     return 'yes';
   }
@@ -13,4 +12,47 @@ const pal = string => {
     return pal(string.substr(1, string.length - 2));
   }
 };
+
 console.log(pal(string));
+
+// _______________________________________________________________________________________________-
+
+const pal2 = splitString => {
+  if (splitString.length <= 1) {
+    return 'yes';
+  }
+  if (splitString[0] !== splitString[splitString.length - 1]) {
+    return 'no';
+  } else {
+    splitString.shift();
+    splitString.pop();
+    return pal2(splitString);
+  }
+};
+
+const func = string => {
+  const splitString = string.split('');
+  return pal2(splitString);
+};
+
+console.log(func(string));
+
+//_____________________________________________________________________________-
+
+const pal3 = string => {
+  const splitString = string.split('');
+
+  if (splitString.length <= 1) {
+    return 'yes';
+  }
+  if (splitString[0] !== splitString[splitString.length - 1]) {
+    return 'no';
+  } else {
+    splitString.shift();
+    splitString.pop();
+    const newString = splitString.join('');
+    return pal(newString);
+  }
+};
+
+console.log(pal3(string));
